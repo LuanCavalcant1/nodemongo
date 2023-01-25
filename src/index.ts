a9f49231145fdad597bf1f009f5a96e6cc32e0c9
Luan Cavalcanti <luanjcavalcanti@gmail.com>
2023-01-25 10:42:55 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
+import express, { Request, Response } from "express";
+import { config } from "dotenv";
 
-console.log('hello world')
+config();
+
+const app = express();
+
+const port = process.env.PORT || 3333;
+
+app.get("/", (request: Request, response: Response) => {
+  response.send("hello world");
+});
+app.listen(port, () => console.log(`listening on port ${port}!`));
